d4932bd14d35d728f494f36c0eb0b6729a6910c7
MattyMyers22 <mmyers2206@gmail.com>
2023-10-08 08:37:16 -0700

    Added python scripts for extracting carisma data

---
+++ gz_extract.py
@@ -0,0 +1,32 @@
+# Extract data from gz files
+
+# Import package
+import gzip
+import os
+
+# Days of data to extract
+days = ['06', '07', '08']
+
+# Root file path to gz files
+root_file_path = 'C:/Users/socce/OneDrive/Documents/Space Apps 2023/2017/09/'
+
+# Read data in gz file
+for day in days:
+    root_day_path = root_file_path + day + '/'
+
+    for filename in os.listdir(root_day_path):
+        if filename.endswith('.gz'):
+            gz_file_path = os.path.join(root_day_path, filename)
+
+            # Create folder to store extracted data
+            extracted_data_folder = root_day_path + day + '_extracted_data/'
+            os.makedirs(extracted_data_folder, exist_ok=True)
+
+            # Extract data from Gzip file
+            with gzip.open(gz_file_path, 'rb') as gz_file:
+                gz_contents = gz_file.read().decode('utf-8')
+
+                # Save extracted data to file
+                extracted_data_file = os.path.join(extracted_data_folder, f"{filename}.txt")
+                with open(extracted_data_file, 'w', encoding='utf-8') as output_file:
+                    output_file.write(gz_contents)
